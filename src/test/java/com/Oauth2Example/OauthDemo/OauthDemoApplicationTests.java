079658558b6d22e375ac0e8cc1ed8d1d5e6cb80d
dvrishabh02 <devrishabhsharma02@gmail.com>
2025-05-10 13:46:59 +0530

    Initial commit

---
+++ src/test/java/com/Oauth2Example/OauthDemo/OauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Oauth2Example.OauthDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
